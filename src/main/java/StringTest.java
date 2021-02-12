7de1dc9880d9f65659526275ebe874e68c52de9b
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-02-12 11:56:22 -0600

    feat: first maven commmit

---
+++ src/main/java/StringTest.java
@@ -0,0 +1,16 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class StringTest {
+    public static void main(String[] args) {
+        System.out.println("Please enter your input here: ");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+
+    }
+}
+//        CheckInt string = Joiner.on("...").skipNulls();
+//        String result = joiner.join("Harry", null, "Ron", "Hermione");
+//        System.out.println("result = " + result);
