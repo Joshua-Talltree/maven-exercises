37d779d818a87d87b6bbc3e85e322b657dcc5649
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-02-18 20:32:24 -0600

    StringTest practice

--- src/main/java/StringTest.java
+++ src/main/java/StringTest.java
@@ -1,5 +1,6 @@
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class StringTest {
@@ -8,12 +9,22 @@ public class StringTest {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
-        if (StringUtils.isNumeric(input)){
+
+//        if (StringUtils.isNumeric(input)) {
+//            System.out.println(input + " is a number");
+//        } else {
+//            System.out.println("is not a number");
+//            System.out.println("Flipped: " + StringUtils.swapCase(input));
+//            System.out.println("Reversed: " + StringUtils.reverse(input));
+//        }
+
+        if (StringUtils.isNumeric(input)) {
             System.out.println(input + " is a number");
         } else {
-            System.out.println("is not a number");
-            System.out.println("Flipped: " + StringUtils.swapCase(input));
-            System.out.println("Reversed: " + StringUtils.reverse(input));
+            System.out.println("is a character");
+            System.out.println("Capitalized " + StringUtils.capitalize(input));
+            System.out.println("All caps " + StringUtils.upperCase(input));
+            System.out.println(StringUtils.normalizeSpace(input));
         }
     }
 }
